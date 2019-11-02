db57c4575c8bc5e8d6a14e957d2a0ff4a119e63d
Ankitkumarjaiswal <57281056+Ankitkumarjaiswal@users.noreply.github.com>
2019-11-02 14:31:20 +0530

    added css js

---
+++ app/static/abc.js
@@ -0,0 +1,46 @@
+$(function(){
+	$.validator.setDefaults({
+		highlight: function(element){
+			$(element)
+			.closest('.form-group')
+			.addClass('has-error')
+		},
+		unhighlight: function(element){
+			$(element)
+			.closest('.form-group')
+			.removeClass('has-error')
+		}
+	});
+	
+	$.validate({
+		rules:
+		{	
+		    password: "required",
+            birthDate: "required",
+            srn: "required",
+			email: {
+				required: true,
+				email: true
+			}
+		},
+			messages:{			
+				email: {
+				required: true,
+				email: true
+			}
+		},
+				password: {
+					required: " Please enter password"
+				},
+				birthDate: {
+					required: " Please enter birthdate"
+				},
+				email: {
+					required: ' Please enter email',
+					email: ' Please enter valid email'
+				},
+				
+			}
+			
+	});
+});
