b434048d95703b46d2a45567f9d403698329fb6f
Anurag Sharan <sharananurag998@gmail.com>
2019-11-02 15:10:41 +0530

    Fixed changes, added REVA_Events.py

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
-FLASK_APP=microblog.py
+FLASK_APP=REVA_Events.py
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,8 @@
-
 from app import app
 from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	    user = {'username': 'Anurag'}
-    return render_template('index.html', title='Home', user=user)
-
+	user = {'username': 'Anurag'}
+	return render_template('index.html', title='Home', user=user)
