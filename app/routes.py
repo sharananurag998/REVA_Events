033dc0394c9bb392fd4ada1e7c5c995d5ef6205a
Anurag Sharan <sharananurag998@gmail.com>
2019-11-02 15:03:57 +0530

    Added index.html

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	    return "Hello, World!"
+	    user = {'username': 'Anurag'}
+    return render_template('index.html', title='Home', user=user)
 
