1bf69d1fdc0d53d656a8a246cb6315fce222df6a
Anurag Sharan <sharananurag998@gmail.com>
2019-11-03 03:40:52 +0530

    Added UserMixin

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from app import db
 from datetime import datetime 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -9,6 +12,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
