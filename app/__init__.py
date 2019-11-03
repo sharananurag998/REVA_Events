103140579c4958b8652b947d990c083b6c4be75b
Anurag Sharan <sharananurag998@gmail.com>
2019-11-03 11:47:07 +0530

    Fixed Forms

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 
 
@@ -12,6 +13,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -37,7 +37,6 @@ def login():
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
-
         return redirect(next_page)
     return render_template('login2.html', title='Sign In', form=form)
 
